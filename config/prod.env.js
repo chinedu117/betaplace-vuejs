6eb092419719523cf42c14a0178fd73a19fbab92
chinedu <chinedudimonyeka@gmail.com>
2019-03-26 22:32:17 +0100

    added netlify.toml file

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  ROOT_API: '"http://api.betaplace.com.ng/"',
+  ROOT_API: '"http://api.betaplace.com.ng/api"',
   ROOT_URL: '"https://www.betaplace.com.ng"',
   GOOGLE_CLIENT_ID: '"41873985302-a6bgttc1vqo4me4vn4ji5r19ourjkda6.apps.googleusercontent.com"',
   GOOGLE_CALLBACK_URL: '"${ROOT_URL}/login/google/callback"',
