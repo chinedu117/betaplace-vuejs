4e980a39cca55ca07f6d65ff5b7c578e3f70667e
chinedu <chinedudimonyeka@gmail.com>
2019-03-26 20:07:52 +0100

    Moved service worker to index.html

--- src/main.js
+++ src/main.js
@@ -76,7 +76,7 @@ Vue.config.productionTip = process.env.NODE_ENV === 'production'
 const prod = process.env.NODE_ENV === 'production'
 const shouldSW = 'serviceWorker' in navigator && prod
 if (shouldSW) {
-  navigator.serviceWorker.register('/service-worker.js').then(() => {
+  navigator.serviceWorker.register('./../service-worker.js').then(() => {
     console.log("Service Worker Registered!")
   })
 }
