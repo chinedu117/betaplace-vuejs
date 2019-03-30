f8f0ae80531e36b85eb732bf9b72f6dda6aa2fcb
chinedu <chinedudimonyeka@gmail.com>
2019-03-30 11:28:46 +0100

    fixed service worker bug

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,7 +3,7 @@ module.exports = {
   NODE_ENV: '"production"',
   ROOT_API: '"https://betaplace-api.com.ng/api"',
   ROOT_URL: '"https://www.betaplace.com.ng"',
-  GOOGLE_CLIENT_ID: '"41873985302-a6bgttc1vqo4me4vn4ji5r19ourjkda6.apps.googleusercontent.com"',
+  GOOGLE_CLIENT_ID: '"41873985302-224fgf0n2hk74ejp413fk01fca5a07s0.apps.googleusercontent.com"',
   GOOGLE_CALLBACK_URL: '"https://www.betaplace.com.ng/login/google/callback"',
   FACEBOOK_CLIENT_ID: '"838567409811629"',
   FACEBOOK_CALLBACK_URL: '"https://www.betaplace.com.ng/login/facebook/callback"',
