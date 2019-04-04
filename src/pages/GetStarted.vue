ad354337e87f4462bd6e65184b1795fc2773c181
chinedu <chinedudimonyeka@gmail.com>
2019-04-04 07:58:57 +0100

    WIP getting started

--- src/pages/GetStarted.vue
+++ src/pages/GetStarted.vue
@@ -28,47 +28,63 @@
              <article>
 
 
-             <h2 class="display-2" id="introduction"><a href="#introduction" class="anc ">Introduction</a></h2>
+             <h2 class="display-1" id="introduction"><a href="#introduction" class="anc ">Introduction</a></h2>
 
 			  	<p>Betaplacae.com.ng is a platform to help you to  rent out your apartment or condomium easily. This guide will help you to package the apartment or condomium  (where we  refer to as “place”), profitably. We assume that you are either the house owner (LandLord) or the agent  (individual ) or agency company  entrusted with the responsibility of marketing or finding potential  tenant.		
 			  	</p>
 
 
-             <h2 class="display-2" id="Requirements"><a href="#Requirements" class="anc ">Requirements</a></h2>
+             <h2 class="display-1" id="Requirements"><a href="#Requirements" class="anc ">Requirements</a></h2>
   		
 				<p>To get started we assume that you are above 18 years old and have the necessary legal backing to upload an apartment to the public. We recommend that the following steps.</p>
 
-               <h4 class="display-1" id="first"><a href="#first" class="anc ">Get permission </a></h4>
+               <h4 class="headline" id="get-permission"><a href="#get-permission" class="">Get permission </a></h4>
                 
 				<p>You need to get permission from the landlord or the care taker of the place you want to rent out. Though our system does not enforce this but we assume that you already have a written or spoken permission to carry on.  The permission is to allow you to take the necessary data.</p>
 
-				<h4 class="display-1" id="first"><a href="#first" class="anc ">Agreement</a></h4>
+				<h4 class="headline" id="agreement"><a href="#agreement" class="">Agreement</a></h4>
 
 				<p>Except you are the landlord, you may need to reach an agreement about  your commission or accruable in cash or in kind from the potential transaction. The percentage or the benefits depends on you. We want  you to succeed as well.</p>
 
-				<h4 class="display-1" id="first"><a href="#first" class="">Data Collection</a></h4>
+				<h2 class="display-1" id="data-collection"><a href="#data-collection" class="anc">Data Collection</a></h2>
 
-				<p>The data to collect is to inform the potential tenant of the nature of the place, the type of apartment, and other relevant  environment information. For example some tenants prefer a quite environment while some may prefer a moderate distraction, some may prefer that filled with gaiety. </p>
+				<p>The data to collect is to inform the potential tenant of the nature of the place, the type of apartment, and other relevant  environment information. For example some tenants prefer a quiet environment while some may prefer a moderate distraction, some may prefer that filled with gaiety. </p>
 
-				<p>Giving accurate information about the place is highly encouraged and benefitial.  Below are some the information that would required.  You will be require to provide the following information.</p>
+				<p>Giving accurate information about the place is highly encouraged and beneficial.  Below are some the information that would required.  You will be require to provide the following information.</p>
                 
-                <h4 class="display-1" id="first"><a href="#first" class="">Apartment Type</a></h4>
+                <h4 class="headline" id="apartment-type"><a href="#apartment-type" class="">Apartment Type</a></h4>
  				<p>See information on our categories of apartment</p>
 				
-				<h4 class="display-1" id="first"><a href="#first" class="">  Price  </a></h4>
-				<p>Here you give the price of the apartment per year. However if the tenant is require to pay a different  amount at first and another subsequenty, you can specity that in the price description field</p>
+				<h4 class="headline" id="first"><a href="#first" class="">  Price  </a></h4>
+				<p>Give the price of the apartment per year. However if the tenant is required to pay a different  amount at first and another subsequently, you can specify that in the price description field</p>
 				
-				<h4 class="display-1" id="first"><a href="#first" class="">Description</a></h4>
- 				<p>This requires a brief summary of the apartment and its features and other things that makes it to compete competitively.</p>
+				<h4 class="headline" id="first"><a href="#first" class="">Description</a></h4>
+ 				<p>A brief summary of the apartment and its features and other things that makes it to compete competitively. You can also mention the type of tenant it may be suitable for.</p>
 
 
-				<h4 class="display-1" id="first"><a href="#first" class="">The location information</a></h4>
+				<h4 class="headline" id="location-information"><a href="#location-information" class="">The location information</a></h4>
 
-				<p> The state, city, address of the place.  The “Mark this point”  button is required to be taken at most 50m from the point. This is to help to make your place discoverable on the map or by our proximity estimation filter system. If for privacy purpose you do not want to declare the exact point the you can take do that at most 50m from the point. Though we try to make sure that we get the accurate location, however we also inform you that the accuracy also depends on the GPS system of  your device.</p>
+				<p> The state, city, address of the place.  The <b>“Mark this point”</b>  button is required to be taken at most 50m from the point. This is to help to make your place discoverable on the map or by our proximity estimation filter system. If for privacy purpose you do not want to declare the exact point the you can take do that at most 50m from the point. Though we try to make sure that we get the accurate location, however we also inform you that the accuracy also depends on the GPS system of  your device.</p>
+                
+                <h4 class="headline" id="features"><a href="#features" class="">Features</a></h4>
+				<p>In this part you simply give the information about the place under any heading of your choice. For  example you can specify the following :</p>
+
+				<p>            
+					            <ul>
+								 	<li>Floor Finishing: Petrified tiles</li>
+								 	<li>Wall finishing:  Aesthetic wall papers</li>
+								 	<li>Eletricity: Has 12 hours light daily or </li>
+								 	<li>Environment:  Serene or moderate distraction or Socially active depending on the situation</li>
+								 	<li>Even as you try to give accurate information, be positive.</li>
+								 </ul>
+				</p>
+
+
+
 
 				<h4 class="display-1" id="first"><a href="#first" class="">Cover Picture</a></h4>
 
-
+               
 
 
 
@@ -138,6 +154,9 @@ article h2:first-of-type
 	margin-top: 35px
 	margin-bottom: 10px
 
+@media screen and (max-width: 1080px)
+	article, header
+		padding: 15px 30px 30px
 </style>
 
     
